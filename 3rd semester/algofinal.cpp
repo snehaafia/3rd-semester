9033c1ed926fb4c1c2dce46973926017e0c75aa2
snehaafia <snehaafia07@gmail.com>
2026-08-01 21:47:24 +0600

    push new updates

---
+++ 3rd semester/algofinal.cpp
@@ -0,0 +1,25 @@
+int fibo (int n)
+{
+    if (n<=1){
+        retuen n;}
+
+            else
+            {
+                return fibo (n-1)+fibo(n-2);
+            }
+}
+
+int main()
+{
+
+    for (i=0;i<=n;i++)
+    {
+        cout<<fibo(i);
+    }
+}
+
+int main ()
+{
+    int n,F[n=1];
+    f[0]
+}
